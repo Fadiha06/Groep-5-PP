0ed8d341e49d3f7439902e5defdd0689c6f0e6df
samerL <samerbq20055@gmail.com>
2026-06-12 22:05:16 +0200

    Update studentRoutes.js

--- DB+API/routes/studentRoutes.js
+++ DB+API/routes/studentRoutes.js
@@ -5,4 +5,7 @@ const { verifyToken, requireRole } = require('../middleware/authMiddleware');
 
 router.get('/dashboard', verifyToken, requireRole('student'), StudentController.getDashboard);
 
+router.get('/logboek', verifyToken, requireRole('student'), StudentController.getLogboek);
+router.post('/logboek/dag', verifyToken, requireRole('student'), StudentController.saveLogboekDag);
+
 module.exports = router;
