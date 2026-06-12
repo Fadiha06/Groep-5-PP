4f52bcbef7cc895be39814f345f99b92d0903f19
denishris-ehb <denis.hristov@student.ehb.be>
2026-06-12 11:10:08 +0200

    merge login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Alle gebruikers ophalen
+
 router.get('/', userController.getUsers);
 
-// Account aanmaken door Admin
+
 router.post('/', userController.createAccount);
 
-// Gebruiker verwijderen
+
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
