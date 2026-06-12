a83ad171c178d94ee7dce90a9c04a5f7c76a8d7a
maxwellitto <maxime.speeckaert@student.ehb.be>
2026-06-12 10:09:04 +0200

    kuis

--- DB+API/routes/stageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const StageController = require('../controllers/stageController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-// Stagevoorstel indienen (vereist inloggen als student)
-router.post('/voorstel', authMiddleware, StageController.submitVoorstel);
-
-module.exports = router;
