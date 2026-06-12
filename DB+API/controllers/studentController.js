0ca92e454dbc8cf096699cf5c04a43c429ee67d3
samerL <samerbq20055@gmail.com>
2026-06-12 21:56:11 +0200

    Update studentController.js

--- DB+API/controllers/studentController.js
+++ DB+API/controllers/studentController.js
@@ -48,6 +48,71 @@ static async getLogboek(req, res) {
 
         res.json({ stageInfo, laasteDag });
 
+        const StudentModel = require('../models/studentModel');
+
+class StudentController {
+
+    static async getDashboard(req, res) {
+        try {
+            const gebruikerId = req.user.id;
+
+            const student = await StudentModel.getStudentByGebruikerId(gebruikerId);
+            if (!student) {
+                return res.status(404).json({ error: 'Studentprofiel niet gevonden' });
+            }
+
+            const [stats, stageproces, logboek, notificaties] = await Promise.all([
+                StudentModel.getDashboardStats(student.student_id),
+                StudentModel.getStageproces(student.student_id),
+                StudentModel.getLogboekDezeWeek(student.student_id),
+                StudentModel.getNotificaties(gebruikerId)
+            ]);
+
+            res.json({
+                student,
+                stats,
+                stageproces,
+                logboekDezeWeek: logboek,
+                notificaties
+            });
+
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Serverfout bij ophalen dashboard' });
+        }
+    }
+
+static async getLogboek(req, res) {
+    try {
+        const gebruikerId = req.user.id;
+
+        const student = await StudentModel.getStudentByGebruikerId(gebruikerId);
+        if (!student) {
+            return res.status(404).json({ error: 'Studentprofiel niet gevonden' });
+        }
+
+        const [stageInfo, laasteDag] = await Promise.all([
+            StudentModel.getLogboekStageInfo(student.student_id),
+            StudentModel.getLaatsteLogboekDag(student.student_id)
+        ]);
+
+        res.json({ stageInfo, laasteDag });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Serverfout bij ophalen logboek' });
+    }
+}
+
+
+
+
+}
+
+module.exports = StudentController;
+
+
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Serverfout bij ophalen logboek' });
