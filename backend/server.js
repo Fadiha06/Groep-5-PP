366537227a860e998098a4471ec660a959c03cfc
denishris-ehb <denis.hristov@student.ehb.be>
2026-06-15 14:33:00 +0200

    issue CORS, added NULL

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:5000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['http://localhost:5000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', 'null'] }));
 app.use(express.json());
 
 // Basic Route
