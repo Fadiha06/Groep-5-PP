c9ba4644622ffc7177dcf16fce64fe8d1f0c3434
phoumy <phoumy.saymoungkhoune@student.ehb.be>
2026-06-17 15:50:51 +0200

    fix: student-routes mounten + competentie middleware

--- backend/routes/competentieRoutes.js
+++ backend/routes/competentieRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const competentieController = require('../controllers/competentieController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { verifyToken } = require('../middleware/authMiddleware');
 
-router.get('/', authMiddleware, competentieController.getAllCompetenties);
-router.post('/', authMiddleware, competentieController.createCompetentie);
-router.put('/:id', authMiddleware, competentieController.updateCompetentie);
-router.delete('/:id', authMiddleware, competentieController.deleteCompetentie);
+router.get('/', verifyToken, competentieController.getAllCompetenties);
+router.post('/', verifyToken, competentieController.createCompetentie);
+router.put('/:id', verifyToken, competentieController.updateCompetentie);
+router.delete('/:id', verifyToken, competentieController.deleteCompetentie);
 
 module.exports = router;
