5630efc7ec029913394370c660156e8ead687e06
phoumy <phoumy.saymoungkhoune@student.ehb.be>
2026-06-18 10:24:52 +0200

    Update docentModel.js

--- backend/models/docentModel.js
+++ backend/models/docentModel.js
@@ -91,16 +91,14 @@ const getDossiers = async (docentId) => {
             b.stad AS bedrijf_stad,
             b.telefoon AS bedrijf_telefoon,
             mg.naam AS mentor_naam,
-            sm.telefoonnummer AS mentor_telefoon,
-            st.startdatum,
-            st.einddatum
+            sm.telefoonnummer AS mentor_telefoon
+            , st.startdatum
+            , st.einddatum
         FROM STAGE st
         JOIN STUDENT s ON s.student_id = st.student_id
         JOIN GEBRUIKER g ON g.id = s.gebruiker_id
         LEFT JOIN BEDRIJF b ON b.bedrijf_id = st.bedrijf_id
-        LEFT JOIN STAGEMENTOR sm
-            ON sm.mentor_id = st.mentor_id
-            OR (st.mentor_id IS NULL AND sm.bedrijf_id = st.bedrijf_id)
+        LEFT JOIN STAGEMENTOR sm ON sm.bedrijf_id = st.bedrijf_id
         LEFT JOIN GEBRUIKER mg ON mg.id = sm.gebruiker_id
         WHERE st.leerkracht_id = ?`,
         [docentId]
